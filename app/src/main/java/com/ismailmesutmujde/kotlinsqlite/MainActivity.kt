e479a64c1580d6277020400a25a12d6f777813f6
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-05 11:35:16 +0300

    updated: sqlite database

--- app/src/main/java/com/ismailmesutmujde/kotlinsqlite/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinsqlite/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ismailmesutmujde.kotlinsqlite
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,5 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        // SQLITE DATABASE
+
+        try {
+            val myDatabase = this.openOrCreateDatabase("Musicians", Context.MODE_PRIVATE, null)
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
